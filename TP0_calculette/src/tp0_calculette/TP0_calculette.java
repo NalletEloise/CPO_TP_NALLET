01906bd50b36504e2597acf9c9e0c225063cf898
Eloise Nallet <eloise.nallet@epfedu.fr>
2025-10-22 12:43:37 +0200

    Créeation d'un nouveau projet TP0_calculette

---
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+/**calculette: calculer
+ * 20/10/25
+ * @author Elna
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        systhem.out.println("""Please enter the operator:
+                                                1) add
+            2) substract
+            3) multiply
+            4) divide
+            5) modulo """);
+    }
+    
+}
