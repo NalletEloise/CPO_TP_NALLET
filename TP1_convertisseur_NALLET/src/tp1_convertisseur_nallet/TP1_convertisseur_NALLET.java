21e298812c698a609b70be985cb132283e98b73f
Eloise Nallet <eloise.nallet@epfedu.fr>
2025-11-03 16:13:49 +0100

    Créeation d'un nouveau projet TP1_stats

--- TP1_convertisseur_NALLET/src/tp1_convertisseur_nallet/TP1_convertisseur_NALLET.java
+++ TP1_convertisseur_NALLET/src/tp1_convertisseur_nallet/TP1_convertisseur_NALLET.java
@@ -4,6 +4,8 @@
  */
 package tp1_convertisseur_nallet;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Elna
@@ -14,7 +16,80 @@ public class TP1_convertisseur_NALLET {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double temp;
+        double tempK;
+        double tempC;
+        double tempC2;
+        double tempF;
+        double tempF2;
+        double tempK2;
+        double choix;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bonjour, saisissez une valeur :");
+        temp=sc.nextDouble();
+        
+        System.out.println("Saisissez la conversion que vous souhaitez effectuer :");
+        System.out.println("1: De Celsius vers Kelvin");
+        System.out.println("2: De Kelvin vers Celsius");
+        System.out.println("3: De Fahrenheit vers Celsius");
+        System.out.println("4: De Celsius vers Fahrenheit");
+        System.out.println("5: De Kelvin vers Fahrenheit");
+        System.out.println("6: De Fahrenheit vers Kelvin");
+        choix=sc.nextDouble();
+        
+        if (choix ==1){
+            tempK=CelciusVersKelvin(temp);
+            System.out.println("Temperature en Kelvin : " + tempK +" K");
+        }
+        
+        if (choix ==2){
+            tempC=KelvinVersCelcius(temp);
+            System.out.println("Temperature en Celcius : " + tempC+" C");
+        }
+        
+        if (choix ==3){
+            tempC2=FarenheitVersCelcius(temp);
+            System.out.println("Temperature en Celsius : " + tempC2+" C");
+        }
+        
+        if (choix ==4){
+            tempF=CelciusVersFarenheit(temp);
+            System.out.println("Temperature en Farenheit : " + tempF+" F");
+        }
+        
+        if (choix ==5){
+            tempF2=KelvinVersFarenheit(temp);
+            System.out.println("Temperature en Farenheit : " + tempF2+" F");
+        }
+        
+        if (choix ==6){
+            tempK2=FarenheitVersKelvin(temp);
+            System.out.println("Temperature en Kelvin : " + tempK2+" K");
+        }
+    } 
+    public static double CelciusVersKelvin (double temp) {
+        double temp_K =temp+273.00;
+        return temp_K ;
     }
-    
+    public static double KelvinVersCelcius (double temp) {
+        double tempC =temp-273.00;
+        return tempC ;
+    }
+    public static double FarenheitVersCelcius (double temp) {
+        double temp_C2 =temp-17.78;
+        return temp_C2 ;
+    }
+    public static double CelciusVersFarenheit (double temp) {
+        double temp_F =temp+32.00;
+        return temp_F ;
+    }
+    public static double KelvinVersFarenheit (double temp) {
+        double temp_F2 =temp-459.7;
+        return temp_F2 ;
+    }
+    public static double FarenheitVersKelvin (double temp) {
+        double temp_K2 =temp+255.372;
+        return temp_K2 ;
+    }
 }
