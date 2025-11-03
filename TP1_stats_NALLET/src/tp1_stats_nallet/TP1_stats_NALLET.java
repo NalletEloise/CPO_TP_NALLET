68611e62102ea1c825e4320315ad2eb98393b416
Eloise Nallet <eloise.nallet@epfedu.fr>
2025-11-03 16:13:05 +0100

    Créeation d'un nouveau projet TP1_stats

--- TP1_stats_NALLET/src/tp1_stats_nallet/TP1_stats_NALLET.java
+++ TP1_stats_NALLET/src/tp1_stats_nallet/TP1_stats_NALLET.java
@@ -4,6 +4,10 @@
  */
 package tp1_stats_nallet;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Elna
@@ -14,7 +18,27 @@ public class TP1_stats_NALLET {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int[] faces = new int[6];
+
+        System.out.print("Combien de lancers voulez-vous effectuer ? ");
+        int m = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int tirage = rand.nextInt(6); 
+            faces[tirage]++;              
+        }
+
+        System.out.println("\nRésultats :");
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Face " + (i + 1) + " : " + faces[i] + " fois");
+        }
+        System.out.println("\nRésultats en pourcentage :");
+        for (int i = 0; i < 6; i++) {
+            double pourcentage = (faces[i] * 100.0) / m;
+            System.out.println("Face " + (i + 1) + " : " + pourcentage + " %");
+        }
     }
-    
 }
