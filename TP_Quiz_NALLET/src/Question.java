49bfdf45883dc0bdccdec4741b98f738ec35ec83
Eloise Nallet <eloise.nallet@epfedu.fr>
2025-11-26 12:30:24 +0100

    TP_Quiz_NALLET

---
+++ TP_Quiz_NALLET/src/Question.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Elna
+ */
+public class Question {
+    private String intitule;
+    private String proposition1;
+    private String proposition2;
+    private String proposition3;
+    private String proposition4;
+    private int indexBonneReponse;
+}
