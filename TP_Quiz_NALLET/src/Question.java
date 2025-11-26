1ce5aa9c7c29b9d14be69202d7b6b92580aa2540
Eloise Nallet <eloise.nallet@epfedu.fr>
2025-11-26 12:49:21 +0100

    TP_Quiz_NALLET

--- TP_Quiz_NALLET/src/Question.java
+++ TP_Quiz_NALLET/src/Question.java
@@ -9,9 +9,36 @@
  */
 public class Question {
     private String intitule;
-    private String proposition1;
-    private String proposition2;
-    private String proposition3;
-    private String proposition4;
-    private int indexBonneReponse;
+    private String prop1;
+    private String prop2;
+    private String prop3;
+    private String prop4;
+    private int iBR;
+    
+    public Question(String inti, String p1, String p2, String p3, String p4, int indBR) {
+        this.intitule=inti;
+        this.prop1=p1;
+        this.prop2=p2;
+        this.prop3=p3;
+        this.prop4=p4;
+        this.iBR=indBR;
+}
+    public String getinti(){
+        return intitule;
+    }
+    public String getp1(){
+        return prop1;
+    }
+    public String getp2(){
+        return prop2;
+    }
+    public String getp3(){
+        return prop3;
+    }
+    public String getp4(){
+        return prop4;
+    }
+    public int getindBR(){
+        return iBR;
+    }
 }
